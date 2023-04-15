017730df4809e3e492025c4ef512abda7be2bcae
Daniel <daniel.pessoa2010@hotmail.com>
2023-04-15 18:18:09 -0300

    Update image.js

--- image.js
+++ image.js
@@ -6,11 +6,6 @@ let imagemCarro3;
 let imagemCarro4;
 let imagemCarro5;
 let imagemCarro6;
-
-let somDaTrilha;
-let somDaColisao;
-let somDoPonto;
-
 function preload() {
     imagemDaEstrada = loadImage("/imagens/estrada.png");
     imagemDoAtor = loadImage("/imagens/ator-1.png");
@@ -22,8 +17,5 @@ function preload() {
     imagemCarro6 = loadImage("/imagens/carro-6.png");
     imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro4, imagemCarro5, imagemCarro6];
 
-    somDaTrilha = loadSound("/sons/trilha.mp3");
-    somDaColisao = loadSound("/sons/colidiu.mp3");
-    somDoPonto = loadSound("/sons/pontos.wav");
 }
 
